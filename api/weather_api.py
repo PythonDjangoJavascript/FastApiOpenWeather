6e938465d6ba2feeecee05c9d56cd2817697eb31
Sayed <>
2021-11-10 00:30:43 +0600

    openWeather api added

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,9 +1,17 @@
+from typing import Optional
+from fastapi import Depends
 from fastapi.routing import APIRouter
+
+from models.location import Location
+from services.open_weather_service import get_weather_api
 
 
 router = APIRouter()
 
 
-@router.get('/api/weather')
-def weather():
-    return "weatehr report"
+@router.get('/api/weather/{city}')
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+
+    report = get_weather_api(loc.city, loc.state, loc.country, units=units)
+
+    return report
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
+from pathlib import Path
+import json
 
 from api import weather_api
 from views import home
+from services import open_weather_service
 
 
 api = FastAPI()
@@ -13,6 +16,7 @@ def configure():
     """Configure the app"""
     # api_key, database etc
     config_routing()
+    config_api_key()
 
 
 def config_routing():
@@ -22,5 +26,22 @@ def config_routing():
     api.include_router(weather_api.router)
 
 
+def config_api_key():
+    """Configure OpenWeatherAPIKey"""
+    file = Path('settings.json').absolute()
+
+    if not file.exists():
+        print(
+            f"WARNING: {file} file not found, you cannot continue, please see settings_template.json")
+        raise Exception(
+            "settings.json file not found, you cannot continue, please see settings_template.json")
+
+    with open('settings.json') as settings_file:
+        settings = json.load(settings_file)
+        open_weather_service.api_key = settings.get('api_key')
+        print(settings.get('api_key'))
+
+
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api, port=8000, host='127.0.0.1')
