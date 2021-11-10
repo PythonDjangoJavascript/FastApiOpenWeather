c05137fa8f69c469db42a21131b4ef26891fefbf
Sayed <>
2021-11-10 09:42:28 +0600

    asyncronasly called weather api

--- api/weather_api.py
+++ api/weather_api.py
@@ -3,15 +3,15 @@ from fastapi import Depends
 from fastapi.routing import APIRouter
 
 from models.location import Location
-from services.open_weather_service import get_weather_api
+from services.open_weather_service import get_weather_api_async
 
 
 router = APIRouter()
 
 
 @router.get('/api/weather/{city}')
-def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
 
-    report = get_weather_api(loc.city, loc.state, loc.country, units=units)
+    report = await get_weather_api_async(loc.city, loc.state, loc.country, units=units)
 
     return report
--- services/open_weather_service.py
+++ services/open_weather_service.py
@@ -1,16 +1,47 @@
-
+import requests
+import httpx
 from typing import Optional
 
 
 api_key: Optional[str] = None
 
 
-def get_weather_api(city: str, state: Optional[str], country: Optional[str], units: str) -> dict:
+async def get_weather_api_async(city: str, state: Optional[str], country: Optional[str], units: str) -> dict:
 
     if state:
         q = f"{city},{state},{country}"
     else:
         q = f"{city},{country}"
 
-    url = f"api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
-    print(url)
+    # url = f"api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}"
+
+    # Now send the async request
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        response.raise_for_status()  # will throw an exception if status goes wrong
+
+    data = response.json()
+    forcast = data['main']  # filtering only main data
+
+    return forcast
+
+
+# def get_weather_api(city: str, state: Optional[str], country: Optional[str], units: str) -> dict:
+
+#     if state:
+#         q = f"{city},{state},{country}"
+#     else:
+#         q = f"{city},{country}"
+
+#     # url = f"api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
+#     url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}"
+
+#     # now send a get request
+#     response = requests.get(url)
+#     response.raise_for_status()  # will throw an exception if status goes wrong
+
+#     data = response.json()
+#     print(data)
+
+#     return data
